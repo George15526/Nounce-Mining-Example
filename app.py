a6007995cbb101cb5e2ff511328fa0f85b92be31
George <152157371+George15526@users.noreply.github.com>
2025-05-09 22:33:08 +0800

    feat: Validate hashCode input to allow only binary digits (0 or 1)

--- app.py
+++ app.py
@@ -16,6 +16,12 @@ def index():
   if request.method == 'POST':
     data = request.form.to_dict()
     hash_code = data['hashCode']
+    
+    for i in hash_code:
+      if i != '0' and i != '1':
+        alert_msg = '只允許輸入0或1，請重新輸入'
+        return render_template('index.html', alert_msg=alert_msg)
+    
     print(hash_code)
     
     length = len(hash_code)
